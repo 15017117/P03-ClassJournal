2df8d8a71bb3cff9b8e2f2e3775c55fd2abc00af
15017117 <15017117@myrp.edu.sg>
2017-05-04 12:33:29 +0800

    Create layout for Row xml

--- app/src/main/java/com/myapplicationdev/android/p03_classjournal/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p03_classjournal/MainActivity.java
@@ -1,13 +1,31 @@
 package com.myapplicationdev.android.p03_classjournal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvC347;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvC347 = (TextView)findViewById(R.id.textViewC347);
+        tvC347.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Weeks week1 = new Weeks("week 1","B");
+                Intent i = new Intent(MainActivity.this,InfoforC347.class);
+                startActivity(i);
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/myapplicationdev/android/p03_classjournal/Weeks.java
@@ -0,0 +1,27 @@
+package com.myapplicationdev.android.p03_classjournal;
+
+/**
+ * Created by 15017117 on 4/5/2017.
+ */
+
+public class Weeks {
+    private String week;
+
+    private String grade;
+
+    public String getWeek() {
+        return week;
+    }
+
+
+
+    public String getGrade() {
+        return getGrade();
+    }
+
+    public Weeks(String week, String grade) {
+        this.week = week;
+
+        this.grade = grade;
+    }
+}
